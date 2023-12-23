8d5eb08e471c9fa894d344e1b7914e2361d75644
dariush <dariushtaghavi9797@gmail.com>
2023-12-23 20:16:13 +0330

    Update BrowserRouter for GitHub Pages

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import App from "./src/App";
+import reportWebVitals from "./src/reportWebVitals";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import store from "./src/redux/store";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
