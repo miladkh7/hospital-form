e328d51db7f15346a083dc1b23254bbd6bc6a989
dariush <dariushtaghavi9797@gmail.com>
2023-12-26 00:35:11 +0330

    Updates

--- src/components/loading/index.js
+++
@@ -1,9 +0,0 @@
-import "./style.scss";
-const Loading = () => {
-  return (
-    <div className="loading-container">
-      <div className="loading"></div>
-    </div>
-  );
-};
-export default Loading;
--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import todoReducer  from "./slices/forms"
-
-export default configureStore({
-  reducer: {
-    todos: todoReducer,
-  },
-});
