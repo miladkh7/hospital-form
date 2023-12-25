1109bd174d3f2d12487647ebcc25cd53938c9f6c
dariush <dariushtaghavi9797@gmail.com>
2023-12-26 01:02:07 +0330

    add basename

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
 
-    <BrowserRouter>
+    <BrowserRouter basename="/hospital-name">
       <React.StrictMode>
         <App />
       </React.StrictMode> 
